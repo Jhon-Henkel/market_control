2f20ba8381f6621762796dd68d6d48939b675b72
jhon <jhowrf@gmail.com>
2025-02-01 19:37:51 -0300

    build(backend): configurando composer, adicionando linters

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    echo 'Hello, World!';
+});
