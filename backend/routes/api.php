37c2f5f0ed3ab649981227a471a3ffd08512bbd3
jhon <jhowrf@gmail.com>
2025-02-02 16:41:11 -0300

    feat(backend): deixando habilitado somente a rota do chat por enquanto

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,5 +4,5 @@ use App\Modules\ChatBot\Controller\ChatBotController;
 use App\Modules\Nfce\Controller\NfceInsertController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/nfce', NfceInsertController::class);
+//Route::post('/nfce', NfceInsertController::class);
 Route::post('/chat/webhook', ChatBotController::class);
