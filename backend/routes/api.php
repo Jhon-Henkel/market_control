451c41b5e5407b5b67878c79c7ba09f845e9b5ce
jhon <jhowrf@gmail.com>
2025-02-03 17:42:17 -0300

    feat(backend): removendo endpoint inicial de api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Modules\ChatBot\Controller\ChatBotController;
-use App\Modules\Nfce\Controller\NfceInsertController;
 use Illuminate\Support\Facades\Route;
 
-//Route::post('/nfce', NfceInsertController::class);
 Route::post('/chat/webhook', ChatBotController::class);
