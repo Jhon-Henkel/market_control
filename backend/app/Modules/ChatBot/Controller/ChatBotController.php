40705f980a7ed1b659925ab5e4c7af103bff118b
jhon <jhowrf@gmail.com>
2025-02-05 07:19:47 -0300

    feat(backend): Criado relatório para última compra

--- backend/app/Modules/ChatBot/Controller/ChatBotController.php
+++ backend/app/Modules/ChatBot/Controller/ChatBotController.php
@@ -79,7 +79,7 @@ readonly class ChatBotController
             return ResponseChat::responseChat($status, $chatId);
         }
 
-        if ($message === '/last-purchases') {
+        if ($message === '/last_purchase') {
             $this->lastPurchaseChatUseCase->execute($chatId);
             return ResponseChat::responseChat(ResponseChatEnum::FinishChat, $chatId);
         }
