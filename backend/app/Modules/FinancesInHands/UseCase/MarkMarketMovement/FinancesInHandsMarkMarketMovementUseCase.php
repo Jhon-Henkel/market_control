e44f0228987779e380d8a305eace16254f6acd2a
jhon <jhowrf@gmail.com>
2025-02-04 21:52:01 -0300

    feat(backend): questionando carteira para marcar o gasto

---
+++ backend/app/Modules/FinancesInHands/UseCase/MarkMarketMovement/FinancesInHandsMarkMarketMovementUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\FinancesInHands\UseCase\MarkMarketMovement;
+
+class FinancesInHandsMarkMarketMovementUseCase
+{
+    public function execute(): array
+    {
+
+        // no endpoint a ser criado no mfp
+        return ['status' => 'ok'];
+    }
+}
