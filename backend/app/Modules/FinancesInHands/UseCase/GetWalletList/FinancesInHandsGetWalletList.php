64fa83531a1df8f8ca9f056690f3b86e789b83a7
jhon <jhowrf@gmail.com>
2025-02-04 21:55:15 -0300

    feat(backend): questionando carteira para marcar o gasto

--- backend/app/Modules/FinancesInHands/UseCase/GetWalletList/FinancesInHandsGetWalletList.php
+++ backend/app/Modules/FinancesInHands/UseCase/GetWalletList/FinancesInHandsGetWalletList.php
@@ -3,13 +3,16 @@
 namespace App\Modules\FinancesInHands\UseCase\GetWalletList;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class FinancesInHandsGetWalletList
 {
     public function execute(): array
     {
+        Log::info('Solicitando lista de carteiras');
         $headers = ['MFP-TOKEN' => config('app.mfp.token')];
         $response = Http::withHeaders($headers)->get(config('app.mfp.url') . 'wallets')->json();
+        Log::info('Resposta: ' . $response);
         if (empty($response)) {
             return [];
         }
